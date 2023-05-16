83533a52f76749ff53a1a6cc343dff7291493357
Heloísa Tambara <81993336+heloisatambara@users.noreply.github.com>
2023-05-16 10:54:15 -0300

    Delete main directory

--- main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    dob = models.DateField()
-    sex = models.BooleanField()
