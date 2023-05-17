4ebe3df2e22c6fd9d66feb32be6885aacf50556f
Heloisa Tambara <heloisatambara@usp.br>
2023-05-17 09:19:29 -0300

    shows models in html

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    books = Book.objects.all()
+    return render(request, "index.html", {"books": books})
