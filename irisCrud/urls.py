f273faa26e7edc95fe7e52eb22b933b5fe7fadde
Heloisa Tambara <heloisatambara@usp.br>
2023-05-17 09:04:31 -0300

    create html for the new url

--- irisCrud/urls.py
+++ irisCrud/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shelf/', include('shelf.urls')),
+    path('books/', include('books.urls')),
 ]
